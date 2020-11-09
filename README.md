9eee093e0d35734c0d5188fc33bd80550be91778
Shiva Duraisamy <sduraisamy@tesla.com>
2020-11-08 18:48:20 -0600

    Adding the basic build infrastructure to create, build and run RUST code on HOST

---
+++ README.md
@@ -0,0 +1,24 @@
+# RUST-FUN
+
+Run the setup.sh script to clean, build, create a project and link the exe
+    
+    Usage of setup.sh
+
+    -h | --help                     Prints the help menu
+    -i | --install                  Installs the rust compiler and cargo
+    -c | --clean                    Cleans the build directory
+    -b | --build                    Builds all the apps
+    -l | --links                    Creates a symbolic link of all the executable in the executables dir
+    -p | --project <project-name>   Creates a project folder with the provided name based on the simple hello-world rust program
+    
+Example usage of the `setup.sh`:
+    
+    # The following command would create a project called assign-1, build and link the exe in the "executables" directory
+    ➜ ./setup.sh -cbl -p assign-1
+    
+**NOTE: Currently, all above mentioned functionality will only work on macOS**
+
+## LICENCE
+SPDX short identifier: BSD-3-Clause
+
+let the fun begin!!! `¯\_(ツ)_/¯`
---
+++ setup.sh
@@ -0,0 +1,178 @@
+#!/bin/bash
+
+getopt --test > /dev/null
+if [[ $? -ne 4 ]]; then
+    printf "${0} uses get-opt(); on ubuntu install util-linux package / on mac install gnu-getopt\n"
+    exit 1
+fi
+
+which gsed > /dev/null
+if [[ $? -ne 0 ]]; then
+    printf "${red}Install gnu-sed in mac${normal}\n"
+fi
+
+# Exit the script as soon as a command fails
+# set -exit
+set -o pipefail
+
+# Global variables
+red=$'\e[1;31m'
+green=$'\e[1;32m'
+yellow=$'\e[1;33m'
+blue=$'\e[1;34m'
+magenta=$'\e[1;35m'
+cyan=$'\e[1;36m'
+normal=$'\e[0m'
+bold=$'\e[1m'
+
+gs_install=false
+gs_clean=false
+gs_build=false
+gs_sym_links=false
+gs_create_project=false
+gs_project_name=""
+
+usage() {
+    printf ${yellow}
+    printf "Usage of $(basename ${0})\n"
+    printf "\n"
+    printf "%s | %-25s  %-5s\n" "-h" "--help"                   "Prints the help menu"
+    printf "%s | %-25s  %-5s\n" "-i" "--install"                "Installs the rust compiler and cargo"
+    printf "%s | %-25s  %-5s\n" "-c" "--clean"                  "Cleans the build directory"
+    printf "%s | %-25s  %-5s\n" "-b" "--build"                  "Builds all the apps"
+    printf "%s | %-25s  %-5s\n" "-l" "--links"                  "Creates a symbolic link of all the executable in the executables dir"
+    printf "%s | %-25s  %-5s\n" "-p" "--project <project-name>" "Creates a project folder with the provided name based on the template"
+    printf "${normal}\n"
+    exit 0
+}
+
+#parameter parsing
+short_options=h,i,c,b,l,p:
+long_options=help,install,clean,build,links,project:
+
+script_options=$(getopt --options=${short_options} --longoptions=${long_options} --name "${0}" -- "$@")
+if [[ $? -ne 0 ]]; then
+    #getopt error
+    exit 2
+fi
+
+# Necessary for proper parsing of getopt results
+eval set -- "${script_options}"
+
+# Primary bash argumenst parsing loop; new arguments are added as a switch parameter
+while true ; do
+    case "${1}" in
+    -h|--help)
+        usage
+        ;;
+    -i|--install)
+        gs_install=true
+        break
+        ;;
+    -c|--clean)
+        printf "${cyan}Cleaning the build directory${normal}\n"
+        gs_clean=true
+        rm -rf ./build/*
+        rm -rf ./executables/*
+        shift
+        ;;
+    -b|--build)
+        gs_build=true
+        shift
+        ;;
+    -l|--links)
+        gs_sym_links=true
+        shift
+        ;;
+    -p|--project)
+        gs_create_project=true
+        gs_project_name=$2
+        shift 2
+        ;;
+    --)
+        shift
+        break
+        ;;
+    esac
+done
+
+if [[ ${gs_install} == true ]]; then
+    printf "${green}Installing rustup${normal}\n"
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    rustup update
+
+    cargo_version=$( cargo --version )
+    printf "\nSuccessfully installed Cargo: ${blue}${cargo_version}${normal}\n"
+fi
+
+which cargo > /dev/null
+if [[ $? -ne 0 ]]; then
+    printf "${red}Rust compiler and cargo not installed\n"
+    printf "Run setup.sh -i to install rust and cargo${normal}\n"
+    exit 13
+fi
+
+mkdir -p $PWD/apps
+
+if [[ ! -f $PWD/apps/Cargo.toml ]]; then
+    printf "${yellow}Creating:${normal} ${bold}$PWD/apps/Cargo.toml${normal}\n"
+    printf "# Add the required apps in this file\n" >> "$PWD/apps/Cargo.toml"
+    printf "[workspace]\n" >> "$PWD/apps/Cargo.toml"
+    printf "members = [\n" >> "$PWD/apps/Cargo.toml"
+    printf "]\n" >> "$PWD/apps/Cargo.toml"
+
+fi
+
+if [[ ${gs_clean=} == true ]]; then
+    pushd $PWD/apps > /dev/null
+    cargo clean
+    popd > /dev/null
+    rm -rf ../executables/*
+fi
+
+if [[ ${gs_create_project} == true ]]; then
+    if [[ -d $PWD/apps/${gs_project_name} ]]; then
+        printf "${bold}${gs_project_name}${normal}: ${red}Project already exists${normal}\n"
+    else
+        pushd $PWD/apps > /dev/null
+
+        grep ${gs_project_name} "./Cargo.toml" > /dev/null
+        if [[ $? -ne 0 ]]; then 
+            gsed -i "$ s/]/    \"${gs_project_name}\",/" Cargo.toml
+            printf "]\n" >> "./Cargo.toml"
+        fi
+
+        cargo new ${gs_project_name}
+        if [[ $? -ne 0 ]]; then
+            exit 13
+        fi
+
+        popd > /dev/null
+    fi
+fi
+
+if [[ ${gs_build} == true ]]; then
+    printf "${green}Building all projects${normal}\n"
+    pushd $PWD/apps > /dev/null
+
+    cargo build --workspace
+    if [[ $? -ne 0 ]]; then
+        exit 13
+    else 
+        if [[ ${gs_sym_links} == true ]]; then
+            printf "${yellow}Creating symbolic links of the executables${normal}\n"
+
+            rm -rf ../executables/*
+            mkdir -p ../executables
+            exe_files=($( gfind ./target/debug -executable -type f | grep -v bin | grep -v out | grep -v lock | grep -v deps))
+            for i in "${exe_files[@]}"
+            do 
+                ln -sfn "$PWD/${i}" "../executables/$(basename ${i})"
+            done
+        fi
+    fi
+
+    popd > /dev/null
+fi
+
+exit 0
